e364c9a2bdb6770723cd708eac49f3438cdc4c1d
delower-hasan <dhhasansaha11@gmail.com>
2023-07-29 17:30:26 +0600

    stripe payment addedd

--- src/components/PaymentDetails.jsx
+++ src/components/PaymentDetails.jsx
@@ -183,112 +183,13 @@ const PaymentDetails = () => {
       {showCredit && (
         <>
           <div ref={resultRef} className="credit_card">
-            <form action="">
-              <div className="form_data" id="card-number">
-                <label className="form-label">Card Number</label>
-                <input className="form-control w-100" type="text" name="" />
-              </div>
-
-              <div className="d-lg-flex justify-content-between">
-                <div className="form_data">
-                  <label className="form-label">Expiration Date (MM/ YY)</label>
-                  <div className="d-flex justify-content-between align-items-center gap-2 gap-lg-4">
-                    <select
-                      className="form-select"
-                      aria-label="Default select example"
-                    >
-                      <option selected></option>
-                      <option value="01">January</option>
-                      <option value="02">February</option>
-                      <option value="03">March</option>
-                      <option value="04">April</option>
-                      <option value="05">May</option>
-                      <option value="06">June</option>
-                      <option value="07">July</option>
-                      <option value="08">August</option>
-                      <option value="09">September</option>
-                      <option value="10">October</option>
-                      <option value="11">November</option>
-                      <option value="12">December</option>
-                    </select>
-                    <p>/</p>
-                    <select
-                      className="form-select"
-                      aria-label="Default select example"
-                    >
-                      <option selected></option>
-                      <option value="23">2023</option>
-                      <option value="24">2024</option>
-                      <option value="25">2025</option>
-                    </select>
-                  </div>
-                </div>
-
-                <div className="form_data">
-                  <label className="form-label">CVV/ CVC</label>
-                  <input className="form-control" type="number" name="" />
-                </div>
-              </div>
-
-              <div className="billing_information">
-                <h3>Billing information</h3>
-                <p>Please confirm your billing details to proceed. </p>
-
-                <div className="profile d-flex gap-4">
-                  <img src={profile} alt="" />
-                  <div>
-                    <h4>{user ? user : "Penzie"}</h4>
-                    <p>Personal Account</p>
-                  </div>
-                </div>
-
-                <div className="d-flex bill_input">
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="First Name*"
-                  />
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Last Name*"
-                  />
-                </div>
-
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="Address (P.O. box, company name, c/o)*"
-                />
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="Address line 2 (Apartment, suite, unit)"
-                />
-
-                <div className="d-flex bill_input">
-                  <input
-                    className="form-control"
-                    type="number"
-                    placeholder="Postal Code*"
-                  />
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Country*"
-                  />
-                </div>
-              </div>
-
-              <div className="submit_btn">
-                <button type="submit">Pay Now</button>
-              </div>
-            </form>
-          </div>
-
-          <Elements stripe={stripePromise}>
+          
+            <Elements stripe={stripePromise}>
             <PaymentForm />
           </Elements>
+          </div>
+
+        
         </>
       )}
       {showPaypal && (
--- src/utils/payments/CheckoutForm.jsx
+++ src/utils/payments/CheckoutForm.jsx
@@ -1,25 +1,51 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import {CardNumberElement, CardExpiryElement, CardCvcElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import { createPaymentIntent } from "./createPayment";
 import CustomCard from "./CustomCard";
 import { useState } from "react";
 
+// padding: 25px 20px;
+//     border-radius: 8px;
+//     border: 1px solid #444;
+//     background: #121212;
+//     color: #fff;
+
+const CARD_OPTIONS = {
+  iconStyle: 'solid',
+  style: {
+    base: {
+      borderStyle:"solid",
+      borderWidth:'1px',
+      background:'#121212',
+      color:"#fff",
+      border: '1px solid red',
+      fontSize:'20px',
+      width:'100%'
+    },
+    invalid: {
+      iconColor: '#ffc7ee',
+      color: '#ffc7ee',
+    },
+  },
+};
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
 
-  const [cardNumber, setCardNumber] = useState();
-  const [cardExpMonth, setCardExpMonth] = useState();
-  const [cardExpYear, setCardExpYear] = useState();
-  const [cardCvc, setCardCvc] = useState();
+  const [error, setError] = useState();
+
+
+
+
 
   const handlePayment = async () => {
+  
     // Get the PaymentIntent client secret from your server or set it on the client-side
     // const clientSecret = 'YOUR_CLIENT_SECRET';
     if (!stripe) {
       // Stripe.js has not loaded yet, so do nothing.
       return;
     }
-    const cardElement = elements.getElement(CardElement);
+    const cardElement = elements.getElement(CardNumberElement);
 
     const clientSecret = await createPaymentIntent(5000, "usd");
 
@@ -43,7 +69,10 @@ const PaymentForm = () => {
       }
     );
 
+    console.log('paymentdone ',paymentIntent)
+
     if (error) {
+      setError(error?.message)
       console.error(error);
       // Handle error: Show error to the user
     } else {
@@ -53,25 +82,85 @@ const PaymentForm = () => {
 
   return (
     <div>
-      {/* Your other form fields (e.g., name, address) */}
-      {/* Payment Sheet trigger */}
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
-              },
-            },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button onClick={handlePayment}>Pay with Stripe</button>
+                 {
+                error && (
+                  <div className="toast-body text-white fs-5">
+                    {error}
+                  </div>
+                )
+               }
+              <div className="form_data" id="card-number">
+                <label className="form-label">Card Number</label>
+                <CardNumberElement options={CARD_OPTIONS} />
+              </div>
+
+              <div className="d-lg-flex justify-content-between">
+                <div className="form_data">
+                  <label className="form-label">Expiration Date (MM/ YY)</label>
+                  <CardExpiryElement options={CARD_OPTIONS} />
+                </div>
+
+                <div className="form_data">
+                  <label className="form-label">CVV/ CVC</label>
+                  <CardCvcElement options={CARD_OPTIONS} />
+                </div>
+              </div>
+
+              <div className="billing_information">
+                <h3>Billing information</h3>
+                <p>Please confirm your billing details to proceed. </p>
+
+                {/* <div className="profile d-flex gap-4">
+                  <img src={profile} alt="" />
+                  <div>
+                    <h4>{user ? user : "Penzie"}</h4>
+                    <p>Personal Account</p>
+                  </div>
+                </div> */}
+
+                <div className="d-flex bill_input">
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="First Name*"
+                  />
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="Last Name*"
+                  />
+                </div>
+
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Address (P.O. box, company name, c/o)*"
+                />
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Address line 2 (Apartment, suite, unit)"
+                />
+
+                <div className="d-flex bill_input">
+                  <input
+                    className="form-control"
+                    type="number"
+                    placeholder="Postal Code*"
+                  />
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="Country*"
+                  />
+                </div>
+              </div>
+
+              <div className="submit_btn">
+                <button type="button" disabled={!stripe} onClick={handlePayment}>Pay Now</button>
+              </div>
+      
+      {/* <button disabled={!stripe} onClick={handlePayment}>Pay with Stripe</button> */}
     </div>
   );
 };
